75ebbd29af71bbf169836e494ff09d7d4cc9bc7f
Your Name <grzegorzklimek77@gmail.com>
2017-12-14 12:41:00 +0100

    cos

---
+++ android/assets/Batch3TEST.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Batch3" tilewidth="2" tileheight="2" tilecount="12877" columns="79">
+ <image source="../../Downloads/transparent PNG/spike D.png" width="159" height="326"/>
+</tileset>
